88b977951b4b1d1928bb1b5445cbe2c7a43bbb7d
Matthew <mkilpatr@FNAL.GOV>
2019-02-19 13:51:55 -0600

    Change to run QCD Smearing

--- python/postprocessing/examples/example_postproc.py
+++ python/postprocessing/examples/example_postproc.py
@@ -4,7 +4,8 @@ import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 from importlib import import_module
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import jetmetUncertaintiesProducer
 
 from  exampleModule import *
-p=PostProcessor(".",["../../../../NanoAOD/test/lzma.root"],"Jet_pt>150","keep_and_drop.txt",[exampleModule()],provenance=True)
+p=PostProcessor(".",["../../../../NanoAOD/test/lzma.root"],"Jet_pt>150","keep_and_drop.txt",[exampleModule(),jetmetUncertaintiesProducer()],provenance=True)
 p.run()
